0132ce46475ea13434c2cf77553927dd8bc0bd77
Bertopf <bertoperez.apf@gmail.com>
2025-06-19 09:54:29 +0200

    feature/header-JS

--- services.js
+++ services.js
@@ -167,3 +167,22 @@ cargarPrendas();
 {
   
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+ const boton = document.querySelector(".button-toggle");
+ const menuLinks = document.querySelector(".menu");
+ boton.addEventListener("click", function() {
+    menuLinks.classList.toggle("open");
+ })
+}
+)
+document.addEventListener("DOMContentLoaded", function() {
+ const boton = document.querySelector(".submenu-toggle");
+ const menuLinks = document.querySelector(".menu2");
+ boton.addEventListener("click", function() {
+    menuLinks.classList.toggle("open");
+ })
+}
+)
+
+

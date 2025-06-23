21af82f4853c7c429cc7d53a16b6d053657416a9
Miguel Ballesteros <mbacebal@gmail.com>
2025-06-23 12:49:49 +0200

    Header updated  + Index fix

--- src/components/Burguer-menu.js
+++ src/components/Burguer-menu.js
@@ -8,3 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });

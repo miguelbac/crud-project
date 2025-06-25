ee86ff747ed49fca11aa8c5c1a7020ae269d68f2
DubinRios <dubinrioseliecerr@gmail.com>
2025-06-25 16:38:28 +0200

    ajustes de estilos taildwin

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,12 @@
-function initMap () {
-    const ubicacion = { lat: 43.5287988, lng: -5.6643789 };
-    const map = new google.maps.Map(document.getElementById("map"), {
+ //Cargar mapa de google 
+ 
+ function initMap() {
+      const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
-        center: ubicacion
-    });
-
-    new google.maps.Marker({
-        position: ubicacion,
-        map: map,
-        title: "Mi negocio"
-    })
-}
+        center: { lat: 43.5287988, lng: -5.6643789 }
+      });
+      const marker = new google.maps.Marker({
+        position: {lat: 43.5287988, lng: -5.6643789},
+        map: map
+      });
+    }
